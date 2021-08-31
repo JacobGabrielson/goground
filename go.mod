6668130fd2c73bc83971e79ffbe56da83e8a120a
jacob <jacob@amazon.com>
2021-08-31 16:20:48 -0700

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/JacobGabrielson/goground
+
+go 1.16
---
+++ template/template.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"text/template"
+)
+
+const (
+	bottlerocketUserData = `
+[settings.kubernetes]
+api-server = "{{.Cluster.Endpoint}}"
+{{if .Cluster.CABundle}}cluster-certificate = "{{.Cluster.CABundle}}"{{end}}
+cluster-name = "{{if .Cluster.Name}}{{.Cluster.Name}}{{end}}"
+{{if .Constraints.Labels }}[settings.kubernetes.node-labels]{{ end }}
+{{ range $Key, $Value := .Constraints.Labels }}"{{ $Key }}" = "{{ $Value }}"
+{{ end }}
+{{if .Constraints.Taints }}[settings.kubernetes.node-taints]{{ end }}
+{{ range $Taint := .Constraints.Taints }}"{{ $Taint.Key }}" = "{{ $Taint.Value}}:{{ $Taint.Effect }}"
+{{ end }}
+`
+)
+
+type cluster struct {
+	Endpoint string
+	CABundle *string
+	Name     string
+}
+
+type constraints struct {
+	Labels []string
+	Taints map[string]string
+}
+
+type Provisioner struct {
+	Cluster     cluster
+	Constraints constraints
+}
+
+var empty string
+
+func main() {
+	fmt.Printf("-> 'template' running\n")
+	t := template.Must(template.New("").Parse(bottlerocketUserData))
+	if err := t.Execute(os.Stdout, Provisioner{
+		Cluster: cluster{
+			Endpoint: "",
+			CABundle: &empty,
+			Name:     "",
+		},
+		Constraints: constraints{
+			Labels: []string{},
+			Taints: map[string]string{},
+		},
+	}); err != nil {
+		log.Fatalf("execute failed, %v", err)
+	}
+}
